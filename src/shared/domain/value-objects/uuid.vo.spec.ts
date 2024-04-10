347d5413e3e38bbf16e7c6aed26bbb921857528e
celsovcsantos <celsovcsantos@gmail.com>
2024-04-10 01:08:06 +0000

    feat: criando objeto de valor para uuid

---
+++ src/shared/domain/value-objects/uuid.vo.spec.ts
@@ -0,0 +1,23 @@
+import { InvalidUuidError, Uuid } from "./uuid.vo";
+import { validate as uuidValidate } from "uuid";
+
+describe("Uuid Unit Tests", () => {
+  const validateSpy = jest.spyOn(Uuid.prototype as any, "validate");
+  test("should throw error when uuid is invalid", () => {
+    expect(() => new Uuid("invalid-uuid")).toThrowError(new InvalidUuidError());
+    expect(validateSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test("should create a valid uuid", () => {
+    const uuid = new Uuid();
+    expect(uuid).toBeDefined();
+    expect(uuidValidate(uuid.id)).toBe(true);
+    expect(validateSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test("should accept a valid uuid", () => {
+    const uuid = new Uuid("f6f5e9b0-5e8b-4f9d-9b5e-3f7b7f1c4e5d");
+    expect(uuid.id).toBe("f6f5e9b0-5e8b-4f9d-9b5e-3f7b7f1c4e5d");
+    expect(validateSpy).toHaveBeenCalledTimes(1);
+  });
+});
